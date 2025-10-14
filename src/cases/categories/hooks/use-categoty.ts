60e431b5d44d82913482be8938f52a0cfb739aed
Thiago de Oliveira <thiago14thi@outlook.com>
2025-10-13 21:46:04 -0300

    implementaçao case category
    implementaçao case category

---
+++ src/cases/categories/hooks/use-categoty.ts
@@ -0,0 +1,38 @@
+import { CategoryService } from "../services/category.service";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import type { CategoryDTO } from "../dtos/category.dto";
+
+export function useCategories() {
+  return useQuery<CategoryDTO[]>({
+    queryKey: ["categories"],
+    queryFn:  CategoryService.list,
+  });
+}
+
+export function useCategory(id: string) {
+  return useQuery<CategoryDTO>({
+    queryKey: ["categories", id],
+    queryFn: () => CategoryService.getById(id),
+    enabled: !!id // -> or Boolean(id)
+  });
+}
+export function useCreateCategory() {
+  return useMutation<CategoryDTO, Error, Omit<CategoryDTO, "id">>({
+    mutationFn: (category: Omit<CategoryDTO, "id">) => CategoryService.create(category),
+  });
+
+}
+
+export function useUpdateCategory() {
+  return useMutation<CategoryDTO,Error,{ id: string; category: CategoryDTO }>({
+    mutationFn: ({ id, category }) => CategoryService.update(id, category),
+  });
+}
+
+export function useDeleteCategory(){
+  return useMutation<void, Error,string>({
+    mutationFn: (id: string) => CategoryService.delete(id,),
+  });
+}
+
+
---
+++ src/cases/categories/services/category.service.ts
@@ -0,0 +1,31 @@
+import type { CategoryDTO } from '../dtos/category.dto';
+import { api } from '../../../lib/axios';
+
+const _ENDPOINT = '/categories';
+
+export const CategoryService = {
+
+    async list(): Promise<CategoryDTO[]> {
+        const result = await api.get(_ENDPOINT);
+        return result.data;
+    },
+
+    async create(category: CategoryDTO): Promise<CategoryDTO> {
+        const result = await api.post(_ENDPOINT, category);
+        return result.data;
+    },
+
+    async getById(id: string): Promise<CategoryDTO> {
+        const result = await api.get(`${_ENDPOINT}/${id}`);
+        return result.data;
+    },
+
+    async update(id: string, category: CategoryDTO): Promise<CategoryDTO> {
+        const result = await api.put(`${_ENDPOINT}/${id}`, category);
+        return result.data;
+    },
+
+    async delete(id: string): Promise<void> {
+await api.delete(`${_ENDPOINT}/${id}`);
+    }
+};
