4c72e317f6835605522890e01b805e0cda6309e3
Thiago de Oliveira <thiago14thi@outlook.com>
2025-11-03 21:47:09 -0300

    03/11/2025
    03/11/2025

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -50,7 +50,7 @@ function TooltipContent({
         {...props}
       >
         {children}
-        <TooltipPrimitive.Arrow className="bg-foreground fill-foreground z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px]" />
+        <TooltipPrimitive.Arrow className="bg-foreground fill-foreground z-50 size-2.5  rotate-4translate-y-[calc(-50%_-_2px)]5 rounded-[2px]" />
       </TooltipPrimitive.Content>
     </TooltipPrimitive.Portal>
   )
